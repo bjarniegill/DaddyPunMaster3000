15d81f915f5b7fd53c769dcf0a3b7636b30a600f
Bjarni Egill <bjarniegill@gmail.com>
2018-01-05 21:43:37 +0000

    Testing static setup for deploy

--- daddypunmaster3000/settings.py
+++ daddypunmaster3000/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -60,7 +60,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'daddypunmaster3000')
+            os.path.join(BASE_DIR, 'daddypunmaster3000'),
+            os.path.join(PROJECT_DIR, "templates")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -73,6 +74,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'daddypunmaster3000.wsgi.application'
 
@@ -129,5 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__), 'static'), )
+ALLOWED_HOSTS = [".herokuapp.com"]
